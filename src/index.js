cd6e4aaf248627ef65c89b3945892d57586b7507
Erick <thelittlebotengineer@gmail.com>
2023-05-26 23:21:06 -0700

    Update 0.0.01

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//Packages
+const express = require('express');
+
+//Accessing the library/module for usage
+const app = express();
+
+//Server
+const port = process.env.PORT || 3000;
+
+//Port hosted on, as well as logging the status of the server, if it is running or not
+app.listen(port, () => console.log('Server started on port ' + port));
+console.log("Listening on port " + port);
+
+app.use(express.static('public'));
+
+//Limiting the size of json data (1mb) and parsing JSON data
+app.use(express.json({ limit: '5mb' }));
