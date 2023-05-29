7f11e1a72e9d1625310bcc38a2eaae4104cca66d
Erick <thelittlebotengineer@gmail.com>
2023-05-28 17:04:59 -0700

    Update 0.0.200

--- public/Assets/js/login.js
+++ public/Assets/js/login.js
@@ -1,30 +1,54 @@
-const usernameInput = document.getElementById('username');
-const passwordInput = document.getElementById('password');
+window.onload = () => {
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
 
-usernameInput.addEventListener('focus', function() {
-    if (!this.value.trim()) {
-        this.placeholder = '';
-        this.parentElement.classList.add('placeholder-move-up');
-    }
-});
+    usernameInput.addEventListener('focus', function () {
+        if (!this.value.trim()) {
+            this.placeholder = '';
+            this.parentElement.classList.add('placeholder-move-up');
+        }
+    });
 
-usernameInput.addEventListener('blur', function() {
-    if (!this.value.trim()) {
-        this.placeholder = 'Username';
-        this.parentElement.classList.remove('placeholder-move-up');
-    }
-});
+    usernameInput.addEventListener('blur', function () {
+        if (!this.value.trim()) {
+            this.placeholder = 'Username';
+            this.parentElement.classList.remove('placeholder-move-up');
+        }
+    });
 
-passwordInput.addEventListener('focus', function() {
-    if (!this.value.trim()) {
-        this.placeholder = '';
-        this.parentElement.classList.add('placeholder-move-up');
-    }
-});
+    passwordInput.addEventListener('focus', function () {
+        if (!this.value.trim()) {
+            this.placeholder = '';
+            this.parentElement.classList.add('placeholder-move-up');
+        }
+    });
 
-passwordInput.addEventListener('blur', function() {
-    if (!this.value.trim()) {
-        this.placeholder = 'Password';
-        this.parentElement.classList.remove('placeholder-move-up');
+    passwordInput.addEventListener('blur', function () {
+        if (!this.value.trim()) {
+            this.placeholder = 'Password';
+            this.parentElement.classList.remove('placeholder-move-up');
+        }
+    });
+}
+
+
+function LogCheck() {
+    let usernameInput = document.getElementById('username');
+    let passwordInput = document.getElementById('password');
+
+    if (usernameInput.value == "") {
+        usernameInput.style.borderBottomColor = "red";
     }
-});
+    if (passwordInput.value == "") {
+        passwordInput.style.borderBottomColor = "red";
+    }
+
+    if (usernameInput.value != "" && passwordInput.value != "") {
+        if (usernameInput.value == "Erick Tran" && passwordInput.value == "admin") {
+            window.location.href = "/";
+        }
+        if (usernameInput.value.indexOf("student.auhsd.us") != -1 && passwordInput.value != "") {
+            window.location.href = "/";
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // index.js
-
+const fs = require('fs');
 
 require('dotenv').config({ path: './src/credentials.env' });
 /*  EXPRESS */
@@ -70,6 +70,37 @@ app.get('/auth/google/callback',
   function (req, res) {
     // Successful authentication, redirect success.
     if(userProfile._json.hd == "student.auhsd.us" || userProfile._json.hd == "auhsd.us") {
+      
+      let sessionIDCreate = userProfile.id;
+      let newSessionID = "";
+      function encryptSessionID() {
+        for (let i = 0; i < sessionIDCreate.length; i++) {
+          switch (sessionIDCreate[i]) {
+            case "1": newSessionID += "&$*M<%"; break;
+            case "2": newSessionID += "@@F$>."; break;
+            case "3": newSessionID += "@(&F$f"; break;
+            case "4": newSessionID += "#!NF*#"; break;
+            case "5": newSessionID += "@#!F?@"; break;
+            case "6": newSessionID += "(%)^"; break;
+            case "7": newSessionID += "]}T~~$"; break;
+            case "8": newSessionID += "H`G^&>"; break;
+            case "9": newSessionID += "KJH^,&"; break;
+            case "0": newSessionID += "F!,%^/"; break;
+        }
+      }
+    }
+      encryptSessionID();
+      let JSONdata = {
+        "displayName": userProfile.displayName,
+        "firstName": userProfile.name.givenName,
+        "lastName": userProfile.name.familyName,
+        "email": userProfile.emails[0].value,
+        "profilePicture": userProfile.photos[0].value,
+        "id": userProfile.id,
+        "hd": userProfile._json.hd,
+        "session_id": newSessionID
+      }
+      fs.appendFile("./src/studentinformation.json", JSON.stringify(JSONdata), function(err) {console.log(err)});
       res.redirect('/');
     } else {
       res.redirect('/User/Authentication/Log-In').send("You are not a student");
